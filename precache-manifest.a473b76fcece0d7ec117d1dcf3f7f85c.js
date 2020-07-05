a02e88de9a8e54a9565886c0e1f1aa377a4e3f7c
MD-REHMAN <Rehman.2468@gmail.com>
2020-07-06 01:29:02 +0530

    Updates

---
+++ precache-manifest.a473b76fcece0d7ec117d1dcf3f7f85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2acc00c2e3b97028c2f1b0937ebb4d0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8cdcd743c5d3fb8d87a",
+    "url": "/static/css/main.60371105.chunk.css"
+  },
+  {
+    "revision": "2bb186c32c7277f57b52",
+    "url": "/static/js/2.355dee46.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.355dee46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cdcd743c5d3fb8d87a",
+    "url": "/static/js/main.603a155d.chunk.js"
+  },
+  {
+    "revision": "fe634446845eed90d6d0",
+    "url": "/static/js/runtime-main.409e58be.js"
+  }
+]);
